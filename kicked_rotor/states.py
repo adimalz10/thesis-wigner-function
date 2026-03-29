0672955a19c0eb253b70f4089f9e2b7351932ab5
adimalz10 <AdityaLM2004@gmail.com>
2026-03-29 18:56:49 +0530

    More initial states: completely random, q-eigenstate, and coherent state

--- kicked_rotor/states.py
+++ kicked_rotor/states.py
@@ -12,3 +12,35 @@ def localized_state(N, frac=(0.35, 0.65)):
     psi /= np.linalg.norm(psi)
 
     return psi
+
+def q_eigenstate(N):
+    
+    psi = np.zeros(N, dtype = complex)
+
+    psi[np.random.randint(0, N-1)] = 1
+
+    return psi
+
+def random_state(N):
+    
+    psi = np.random.rand(N)
+
+    return psi/np.linalg.norm(psi)
+
+def coherent_state(N, Z = -0.1j, p = 0.5, q = 0.5, M = 20):
+
+    # parameters for creating the state
+    kappa1 = 2*np.pi*np.random.uniform()
+    kappa2 = 2*np.pi*np.random.uniform()
+    hbar = 1/(2*np.pi*N)
+
+    psi = np.zeros(N, dtype = complex)
+
+    for j in range(N):
+        cj = 0.0
+        for m in range(-M, M + 1):
+            xjm = j/N + kappa2/(2*np.pi*N) - m
+            cj += np.exp(1j*(kappa1*m - (xjm/hbar) - (Z * (xjm-q)**2)/(2*hbar)))
+            
+        psi[j] = cj * np.exp(-1j*p*q/(2*hbar))
+    return psi/np.linalg.norm(psi)
