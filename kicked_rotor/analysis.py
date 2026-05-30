95c7d63bb7bb93b5c1acc9615ac93747b46a3ca2
adimalz10 <AdityaLM2004@gmail.com>
2026-05-30 10:17:44 +0530

    removed tqdm from code and cleaned up a little bit

--- kicked_rotor/analysis.py
+++ kicked_rotor/analysis.py
@@ -1,122 +1,90 @@
 import numpy as np
-from tqdm import tqdm
-
-from .states import localized_state
-from .floquet import diagonalize_floquet_operator, evolve_state
+from .floquet import floquet_kicked_harper, floquet_kicked_rotor, evolve_state
 from .negativity import negativity_from_state
 
 
-def negativity_time_series(N, alpha, beta, kappa, steps, state):
-    """
-    Negativity vs time for fixed parameters.
-    """
-    U = diagonalize_floquet_operator(N, alpha, beta, kappa)
+def negativity_time_series(N, alpha, beta, kappa, steps, state, model):
 
-    assert np.allclose(U.conj().T @ U, np.eye(N), atol=1e-3) , rf"Floquet operator is not unitary for N = {N}, $\alpha = {alpha}$, $\beta = {beta} and $\kappa$ = {kappa}"
+    if model.lower() == "rotor":
+        U = floquet_kicked_rotor(N, alpha, beta, kappa)
+    elif model.lower() == "harper":
+        U = floquet_kicked_harper(N, alpha, beta, kappa)
+    else:
+        print("Model not found")
+
+    assert np.allclose(U.conj().T @ U, np.eye(N), atol=1e-3), rf"Floquet operator is not unitary for N = {N}, $\alpha = {alpha}$, $\beta = {beta} and $\kappa$ = {kappa}"
 
     psi0 = state(N)
 
     neg = np.zeros(steps)
 
-    for t in tqdm(np.arange(steps), desc="coupling", leave = False):
+    for t in np.arange(steps):
         neg[t] = negativity_from_state(psi0)
         psi = U @ psi0
-        assert abs(np.linalg.norm(psi) - 1)<1e-3, "Wavefunction not normalized"
+        assert abs(np.linalg.norm(psi) - 1) < 1e-3, "Wavefunction not normalized"
         psi0 = psi
 
     return neg
 
 
-def coupling_scan(N, kappas, alpha, beta, state, steps = None):
-    """
-    Negativity vs coupling strength.
-    """
+def coupling_scan(N, kappas, alpha, beta, state, model, steps=None):
+
     kappas = list(kappas)
     neg = np.zeros(len(kappas))
 
     psi0 = state(N)
 
-    for i, kappa in enumerate(tqdm(kappas, desc="coupling", leave = False)):
+    for i, kappa in enumerate(kappas):
+        if model.lower() == "rotor":
+            U = floquet_kicked_rotor(N, alpha, beta, kappa)
+        elif model.lower() == "harper":
+            U = floquet_kicked_harper(N, alpha, beta, kappa)
+        else:
+            print("Model not found")
 
-        U = diagonalize_floquet_operator(N, alpha, beta, kappa)
-        psi = evolve_state(U, psi0, steps = steps)
+        psi = evolve_state(U, psi0, steps=steps)
         neg[i] = negativity_from_state(psi)
 
     return neg
 
 
-def system_size_scan(sizes, kappa, alpha, beta, state, steps = None):
-    
+def system_size_scan(sizes, kappa, alpha, beta, state, model, steps=None):
+
     sizes = list(sizes)
     neg = np.zeros(len(sizes))
 
-    for i, N in enumerate(tqdm(sizes, desc="system size", leave = False)):
+    for i, N in enumerate(sizes):
         psi0 = state(N)
-        U = diagonalize_floquet_operator(N, alpha, beta, kappa)
-        psi = evolve_state(U, psi0, steps = steps)
+        if model.lower() == "rotor":
+            U = floquet_kicked_rotor(N, alpha, beta, kappa)
+        elif model.lower() == "harper":
+            U = floquet_kicked_harper(N, alpha, beta, kappa)
+        else:
+            print("Model not found")
+        psi = evolve_state(U, psi0, steps=steps)
         neg[i] = negativity_from_state(psi)
 
     return neg
 
-"""def system_size_scan(sizes, kappa, alpha, beta, samples = 80):
 
-    sizes = list(sizes)
-    neg = np.zeros(len(sizes))
-
-    for i, N in enumerate(tqdm(sizes, desc="system size")):
-
-        U = diagonalize_floquet_operator(N, alpha, beta, kappa)
-        psi = localized_state(N)
-
-        tH = N
-        dt = max(1, tH // samples)
-
-        neg_sum = 0
-        count = 0
-
-        for t in range(tH):
-
-            if t % dt == 0:
-                neg_sum += negativity_from_state(psi)
-                count += 1
-
-            psi = U @ psi
-
-        neg[i] = neg_sum / count
-
-    return neg"""
-
-
-def symmetry_scan(alphas, betas, N, kappa, state, steps = None):
-    """
-    Negativity after time evolution for a grid of (alpha, beta).
-
-    Returns
-    -------
-    neg : ndarray (len(alphas), len(betas))
-        Negativity for each symmetry parameter pair.
-    """
+def symmetry_scan(alphas, betas, N, kappa, state, model, steps=None):
 
     alphas = list(alphas)
     betas = list(betas)
 
     neg = np.zeros((len(alphas), len(betas)))
 
-    pbar = tqdm(total=len(alphas) * len(betas), desc="(alpha,beta) scan", leave = False)
-
     psi0 = state(N)
 
     for i, alpha in enumerate(alphas):
         for j, beta in enumerate(betas):
-
-            U = diagonalize_floquet_operator(N, alpha, beta, kappa)
-
-            psi = evolve_state(U, psi0, steps = steps)
-
+            if model.lower() == "rotor":
+                U = floquet_kicked_rotor(N, alpha, beta, kappa)
+            elif model.lower() == "harper":
+                U = floquet_kicked_harper(N, alpha, beta, kappa)
+            else:
+                print("Model not found")
+            psi = evolve_state(U, psi0, steps=steps)
             neg[i, j] = negativity_from_state(psi)
-
-            pbar.update(1)
-
-    pbar.close()
 
     return neg
